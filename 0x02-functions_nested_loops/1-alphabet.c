13defe2d07602ad993a925f425c2ef6864fcdd98
KwambokaN <nyatigonancy@gmail.com>
2023-02-21 08:51:04 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - check the code
+*Description:
+*Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z' n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
