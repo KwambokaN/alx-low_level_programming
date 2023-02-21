5998ae6f6a092a329a2f0478c3dd590a03387a85
KwambokaN <nyatigonancy@gmail.com>
2023-02-20 18:12:59 -0800

    print lowercase alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: 'print alphabets'
- * return: Always 0 (success)
+ *main - Entry point
+ *Description: 'print alphabets'
+ *Return: Always 0 (success)
  */
-
 int main(void)
 {
 	int n = 97;
 
 	while (n <= 122)
+	{
 
 	if (n == 101 || n == 113)
 		{
 	n++;
 	continue;
+		}
+	putchar(n);
+	n++;
 
 		}
 	putchar('\n');
