4665f836d3f35130d414f908711485f4db1c1994
KwambokaN <nyatigonancy@gmail.com>
2023-02-22 10:11:39 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
-
 #include "main.h"
+
+
 /**
 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
 * followed by a new line
 */
-	int main(void);
-
 	void print_alphabet_x10(void)
 	{
 		char ch;
@@ -27,3 +26,4 @@
 			i++;
 		}
 	}
+
