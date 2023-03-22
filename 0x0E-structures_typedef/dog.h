e8c8db8e24708004b6697474d922d9547c939df6
KwambokaN <nyatigonancy@gmail.com>
2023-03-22 06:27:11 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+#include <stdio.h>
+/**
+ * define a new struct
+ * struct to include name age and owner
+ */
+
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+};
+#endif
