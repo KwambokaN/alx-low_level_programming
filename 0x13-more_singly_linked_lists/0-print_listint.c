2f810e8ad902644634d2ca287c8e846cdaec3388
KwambokaN <nyatigonancy@gmail.com>
2023-04-04 12:15:41 -0700

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,15 +5,15 @@
  * @h: pointer to the head of the list.
  * return number of nodes
  */
-
 size_t print_listint(const listint_t *h);
 
 {
 	size_t number = 0;
 
 	while (h)
-}
-{number++
+
+{
+	number++
 	printf("%d\n", h->n);
 h = h->next;
 }
