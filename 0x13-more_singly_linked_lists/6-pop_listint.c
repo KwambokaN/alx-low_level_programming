f1c30f10448cc307e05d0dae7105ed3bcd094a97
KwambokaN <nyatigonancy@gmail.com>
2023-05-02 15:10:57 -0700

    task6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,8 +7,7 @@
  * @n: head nodes data.
  * Return: the head node’s data or 0 if list is empty.
  */
-int pop_listint(listint_t **head);
-
+int pop_listint(listint_t **head)
 {
 listint_t *temp;
 int num;
