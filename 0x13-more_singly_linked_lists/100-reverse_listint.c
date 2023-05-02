83e746bf0643c2e9b9c440544633dc61dfdf9535
KwambokaN <nyatigonancy@gmail.com>
2023-05-02 15:28:21 -0700

    task100

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,7 +3,7 @@
 /**
  * reverse_listint - reverses a linked list
  *
- * @head: pointer to the first element in the list
+ * @head: pointer to the first node in the list
  * Return: pointer to the first node in the new list
  */
 listint_t *reverse_listint(listint_t **head)
