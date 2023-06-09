1f0764ca6c95db581c394c50834d908fb9bc1043
KwambokaN <nyatigonancy@gmail.com>
2023-06-09 12:26:08 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - a function that prints number of arguments passed to it
+* @argc: number of arguments
+* @argv: array of listed  commands 
+* Return: Always 0 (Success)
+*/
+int main(int argc , char *argv[])
+{
+	printf("%d/n" , argc);
+	Return (0);
