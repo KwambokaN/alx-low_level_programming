4b87d678e8cb8a4d010933febcdfb274203f91b3
KwambokaN <nyatigonancy@gmail.com>
2023-06-13 12:07:43 -0700

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,7 +5,7 @@
 #include "main.h"
 
 /**
-* _strdup -  function returns a pointer to a new string which is a duplicate of the string str
+* _strdup -returns a pointer to  a duplicate of the string
 * @str: char
 * Return: 0
 */
